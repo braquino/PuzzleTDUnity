837c052bf782c3b5c1413c549a7cf27dd8f732bc
Bruno Aquino <br_aquino@yahoo.com.br>
2017-12-09 20:00:18 -0200

    add sprites

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+
+	private Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator> ();
+		_animator.SetTrigger ("WalkUp");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(600, 600, 0), 1f);
+	}
+}
